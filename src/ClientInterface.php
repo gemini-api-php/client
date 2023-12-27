a1bb3d625f875673beae5bbd5c1fc2a4b0bb07fa
erdemkose <erdem.kose@nordsec.com>
2023-12-27 03:35:25 +0100

    Add embedding methods in ClientInterface.php

--- src/ClientInterface.php
+++ src/ClientInterface.php
@@ -6,8 +6,10 @@ namespace GeminiAPI;
 
 use GeminiAPI\Enums\ModelName;
 use GeminiAPI\Requests\CountTokensRequest;
+use GeminiAPI\Requests\EmbedContentRequest;
 use GeminiAPI\Requests\GenerateContentRequest;
 use GeminiAPI\Responses\CountTokensResponse;
+use GeminiAPI\Responses\EmbedContentResponse;
 use GeminiAPI\Responses\GenerateContentResponse;
 use GeminiAPI\Responses\ListModelsResponse;
 
@@ -20,7 +22,9 @@ interface ClientInterface
 
     public function countTokens(CountTokensRequest $request): CountTokensResponse;
     public function generateContent(GenerateContentRequest $request): GenerateContentResponse;
+    public function embedContent(EmbedContentRequest $request): EmbedContentResponse;
     public function generativeModel(ModelName $modelName): GenerativeModel;
+    public function embeddingModel(ModelName $modelName): EmbeddingModel;
     public function listModels(): ListModelsResponse;
     public function withBaseUrl(string $baseUrl): self;
 }
