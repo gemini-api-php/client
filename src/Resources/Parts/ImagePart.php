54d9251d691c29bec532b0342fc062744df37866
Astrit Aslani <astrit@hemmer.ch>
2025-01-13 11:34:35 +0100

    ImagePart extend FilePart to keep compatibility and add content functions

--- src/Resources/Parts/ImagePart.php
+++ src/Resources/Parts/ImagePart.php
@@ -4,39 +4,6 @@ declare(strict_types=1);
 
 namespace GeminiAPI\Resources\Parts;
 
-use GeminiAPI\Enums\MimeType;
-use JsonSerializable;
-
-use function json_encode;
-
-class ImagePart implements PartInterface, JsonSerializable
+class ImagePart extends FilePart
 {
-    public function __construct(
-        public readonly MimeType $mimeType,
-        public readonly string $data,
-    ) {
-    }
-
-    /**
-     * @return array{
-     *     inlineData: array{
-     *         mimeType: string,
-     *         data: string,
-     *     },
-     * }
-     */
-    public function jsonSerialize(): array
-    {
-        return [
-            'inlineData' => [
-                'mimeType' => $this->mimeType->value,
-                'data' => $this->data,
-            ],
-        ];
-    }
-
-    public function __toString(): string
-    {
-        return json_encode($this) ?: '';
-    }
 }
