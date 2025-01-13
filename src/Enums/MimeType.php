98dfa329de0eadb8b95e94766633d434c9f20da3
Astrit Aslani <astrit@hemmer.ch>
2025-01-13 11:33:30 +0100

    Add FilePart and MimeType for PDF support

--- src/Enums/MimeType.php
+++ src/Enums/MimeType.php
@@ -6,6 +6,7 @@ namespace GeminiAPI\Enums;
 
 enum MimeType: string
 {
+    case FILE_PDF = 'application/pdf';
     case IMAGE_PNG = 'image/png';
     case IMAGE_JPEG = 'image/jpeg';
     case IMAGE_HEIC = 'image/heic';
---
+++ src/Resources/Parts/FilePart.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GeminiAPI\Resources\Parts;
+
+use GeminiAPI\Enums\MimeType;
+use JsonSerializable;
+
+use function json_encode;
+
+class FilePart implements PartInterface, JsonSerializable
+{
+    public function __construct(
+        public readonly MimeType $mimeType,
+        public readonly string $data,
+    ) {
+    }
+
+    /**
+     * @return array{
+     *     inlineData: array{
+     *         mimeType: string,
+     *         data: string,
+     *     },
+     * }
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'inlineData' => [
+                'mimeType' => $this->mimeType->value,
+                'data' => $this->data,
+            ],
+        ];
+    }
+
+    public function __toString(): string
+    {
+        return json_encode($this) ?: '';
+    }
+}
