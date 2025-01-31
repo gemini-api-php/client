a48e61285d82b24117a5c8928dd1e504818f908b
Erdem Köse <erdemkose@gmail.com>
2025-01-31 16:31:02 +0100

    Update supported mime types (#63)
    Update supported mime types

--- src/Enums/MimeType.php
+++ src/Enums/MimeType.php
@@ -6,8 +6,18 @@ namespace GeminiAPI\Enums;
 
 enum MimeType: string
 {
-    case FILE_CSV = 'text/csv';
-    case FILE_PDF = 'application/pdf';
+    case FILE_PDF = 'application/pdf'; // Will not rename to APPLICATION_PDF to keep the backwards compatibility
+    case APPLICATION_JAVASCRIPT = 'application/x-javascript';
+    case APPLICATION_PYTHON = 'application/x-python';
+
+    case TEXT_PLAIN = 'text/plain';
+    case TEXT_HTML = 'text/html';
+    case TEXT_CSS = 'text/css';
+    case TEXT_MARKDOWN = 'text/md';
+    case TEXT_CSV = 'text/csv';
+    case TEXT_XML = 'text/xml';
+    case TEXT_RTF = 'text/rtf';
+
     case IMAGE_PNG = 'image/png';
     case IMAGE_JPEG = 'image/jpeg';
     case IMAGE_HEIC = 'image/heic';
